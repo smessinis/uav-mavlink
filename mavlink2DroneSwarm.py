3e939eb460e57e34d3a6382847e56073203f1ef1
smessinis <stavros.messinis@gmail.com>
2023-04-11 21:18:36 +0300

    Update mavlink2DroneSwarm.py

--- mavlink2DroneSwarm.py
+++ mavlink2DroneSwarm.py
@@ -8,6 +8,7 @@
 
 import time
 from pymavlink import mavutil
+import math
 
 # Set up connection to the first drone
 master1 = mavutil.mavlink_connection('udp:127.0.0.1:14550')
@@ -20,32 +21,53 @@ master1.set_mode('GUIDED', 4) # 4 stands for guided mode
 master2.arducopter_arm()
 master2.set_mode('GUIDED', 4)
 
+# Define a function to calculate the distance between two GPS coordinates
+def distance(lat1, lon1, alt1, lat2, lon2, alt2):
+    R = 6371000 # Radius of the earth in meters
+    dLat = math.radians(lat2 - lat1)
+    dLon = math.radians(lon2 - lon1)
+    a = math.sin(dLat/2) * math.sin(dLat/2) + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dLon/2) * math.sin(dLon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = R * c + abs(alt2 - alt1)
+    return d
+
 # Start following loop
 while True:
     # Get current position of the first drone
     msg = master1.recv_match(type=['GLOBAL_POSITION_INT'], blocking=True)
-    lat = msg.lat / 1e7
-    lon = msg.lon / 1e7
-    alt = msg.alt / 1e3
+    lat1 = msg.lat / 1e7
+    lon1 = msg.lon / 1e7
+    alt1 = msg.alt / 1e3
 
-    # Send set position target command to the second drone
-    master2.mav.set_position_target_global_int_send(
-        0,            # Timestamp
-        1,            # Target system
-        1,            # Target component
-        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,  # Frame
-        0b0000111111000111,  # Type mask
-        int(lat * 1e7),  # Latitude
-        int(lon * 1e7),  # Longitude
-        int(alt * 1e3),  # Altitude
-        0,            # X velocity
-        0,            # Y velocity
-        0,            # Z velocity
-        0,            # X acceleration
-        0,            # Y acceleration
-        0,            # Z acceleration
-        0,            # Yaw
-        0             # Yaw rate
+    # Get current position of the second drone
+    msg = master2.recv_match(type=['GLOBAL_POSITION_INT'], blocking=True)
+    lat2 = msg.lat / 1e7
+    lon2 = msg.lon / 1e7
+    alt2 = msg.alt / 1e3
+
+    # Calculate the distance between the two drones
+    dist = distance(lat1, lon1, alt1, lat2, lon2, alt2)
+
+    # If the distance is less than 2 meters, move the second drone to a safe position
+    if dist < 2:
+        # Move the second drone to a position that is 5 meters away from the first drone
+        lat = lat1 - 5 * (lat2 - lat1) / dist
+        lon = lon1 - 5 * (lon2 - lon1) / dist
+        alt = alt2
+
+        # Send position target to the second drone
+        master2.mav.set_position_target_global_int(
+            int(lat * 1e7),   # Latitude (degrees * 1e7)
+            int(lon * 1e7),   # Longitude (degrees * 1e7)
+            int(alt * 1e3),   # Altitude (meters * 1e3)
+            0,                # X velocity (m/s)
+            0,                # Y velocity (m/s)
+            0,                # Z velocity (m/s)
+            0,                # X acceleration (m/s^2)
+            0,                # Y acceleration (m/s^2)
+            0,                # Z acceleration (m/s^2)
+            0,            # Yaw
+            0             # Yaw rate
     )
 
     # Wait for a short time before getting the position of the first drone again
